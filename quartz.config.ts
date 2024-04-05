2cd556968e2fc08cd662d72d9ad05f1cce64445c
jerm-jerm <whtjazmin@yahoo.com>
2024-04-04 19:43:03 -0700

    Quartz sync: Apr 4, 2024, 7:43 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Te Mystrala Sagas",
+    pageTitle: "The Mystrala Sagas",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
